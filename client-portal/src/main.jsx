7160df32a5a6b7a62c3b2c8d8697761856a932b5
comexrl-arch <comex.rl@gmail.com>
2026-08-23 09:37:42 -0400

    Ajoute espace onboarding coach mobile

--- client-portal/src/main.jsx
+++ client-portal/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import CoachOnboarding from './CoachOnboarding.jsx'
+
+const Root = window.location.pathname === '/coach' ? CoachOnboarding : App
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 )
